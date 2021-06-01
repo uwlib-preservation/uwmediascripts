3e45f1acf6a497d93555dad5d5b67d51f6d3df30
weaver <theandrewjw@gmail.com>
2021-06-01 16:40:27 -0700

    cue and (de)compress

--- bwf2flac.rb
+++ bwf2flac.rb
@@ -1,15 +1,60 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
 require 'json'
+
+
+ARGV.options do |opts|
+  opts.on("-c", "--compress") { Mode = 'compress' }
+  opts.on("-d", "--decompress") { Mode = 'decompress' }
+  opts.parse!
+end
+
+targets = []
+Mode = 'help' unless defined?(Mode)
+
+def compress(target)
+  flacPath = "#{File.dirname(target)}/#{File.basename(target,".*")}" + '.flac'
+  @cuePath = "#{File.dirname(target)}/#{File.basename(target,".*")}" + '.cue'
+  tagCommand = "metaflac --preserve-modtime "
+  if @targetMetadata['media']['track'][0]['extra']['bext_Present'] == "Yes"
+    organization = @targetMetadata['media']['track'][0]['Producer']
+    description = @targetMetadata['media']['track'][0]['Description']
+    date = @targetMetadata['media']['track'][0]['Encoded_Date']
+    sourcemedia = @targetMetadata['media']['track'][0]['Encoded_Library_Settings']
+    contact = 'https://www.lib.washington.edu/'
+    bextCommand = "--set-tag=ORGANIZATION='#{organization}' --set-tag=DESCRIPTION='#{description}' --set-tag=DATE='#{date}' --set-tag=SOURCEMEDIA='#{sourcemedia}' --set-tag=CONTACT='#{contact}' "
+    tagCommand += bextCommand
+  end
+
+  compressCommand = "flac -f --best --keep-foreign-metadata --preserve-modtime --verify #{target}"
+  system(compressCommand)
+  if File.exist?(@cuePath)
+    cueCommand = "--set-tag-from-file=CUESHEET='#{@cuePath}' --import-cuesheet-from='#{@cuePath}' "
+    tagCommand += cueCommand
+  end
+  tagCommand += flacPath
+  system(tagCommand)
+end
+
+def decompress(target)
+  if ! @targetMetadata['media']['track'][0]['extra']['CUESHEET'].nil? && ! File.exist?(@cuePath)
+    tagCommand = "metaflac --preserve-modtime --show-tag=CUESHEET '#{target}'"
+    cueSheet = `#{tagCommand}`
+    open(@cuePath, 'w') do |f|
+      f.puts cueSheet
+    end
+  end
+  `flac -d --keep-foreign-metadata --preserve-modtime --verify #{target}`
+end
+
 target = ARGV[0]
-flacPath = "#{File.dirname(target)}/#{File.basename(target,".*")}" + '.flac'
-bwfMetadata = JSON.parse(`mediainfo --Output=JSON #{target}`)
-organization = bwfMetadata['media']['track'][0]['Producer']
-description = bwfMetadata['media']['track'][0]['Description']
-date = bwfMetadata['media']['track'][0]['Encoded_Date']
-sourcemedia = bwfMetadata['media']['track'][0]['Encoded_Library_Settings']
-contact = 'https://www.lib.washington.edu/'
+@cuePath = "#{File.dirname(target)}/#{File.basename(target,".*")}" + '.cue'
+@targetMetadata = JSON.parse(`mediainfo --Output=JSON #{target}`)
 
-compressCommand = "flac -f --best --keep-foreign-metadata --preserve-modtime --verify #{target}"
-tagCommand = "metaflac --preserve-modtime --set-tag=ORGANIZATION='#{organization}' --set-tag=DESCRIPTION='#{description}' --set-tag=DATE='#{date}' --set-tag=SOURCEMEDIA='#{sourcemedia}' --set-tag=CONTACT='#{contact}' #{flacPath}"
-
-system(compressCommand)
-system(tagCommand)
+if Mode == 'compress'
+  compress(target)
+elsif Mode=='decompress'
+  decompress(target)
+end
+    

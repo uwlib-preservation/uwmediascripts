717e75e02052c1381b18f44307a8644f14836a21
privatezero <theandrewjw@gmail.com>
2021-08-11 10:05:51 -0700

    update for windows

--- batchbext.rb
+++ batchbext.rb
@@ -1,6 +1,7 @@
 targets = []
 scriptDir = __dir__
 ARGV.each do |input|
+  input = input.tr("\\","/")
   if File.file?(input) && File.extname(input).downcase == '.wav'
     targets << input
   elsif File.directory?(input)
@@ -13,6 +14,8 @@ end
 
 targets.each do |waveFile|
   parsedName = waveFile.split("_")
-  itemNumber =  parsedName[2]
-  system("#{scriptDir}/uwmetaedit2", '-i', itemNumber, waveFile )
+  itemNumber =  File.basename(parsedName[2], ".*")
+  puts "Embedding metadata in #{waveFile}"
+  puts "Identified Item Number: #{itemNumber}"
+  system("ruby", "#{scriptDir}/uwmetaedit2", '-i', itemNumber, waveFile )
 end

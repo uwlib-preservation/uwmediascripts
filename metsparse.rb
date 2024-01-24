d4a5da03641e0021e6cd3f948f240c44fec1ae94
Andrew Weaver <theandrewjw@gmail.com>
2024-01-24 09:10:16 -0800

    add metsparse

---
+++ metsparse.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+
+def parseMETS(input)
+  fileManifest = []
+  metsXML =  Nokogiri::XML(File.read(input))
+  metsXML.remove_namespaces!
+  files = metsXML.xpath("//originalName")
+  files.each {|file| fileManifest << File.basename(file.inner_text)}
+  fileManifest << "\n"
+end
+
+target = ARGV[0]
+outputArray = []
+@metsManifests = Dir.glob("#{target}/*.xml")
+
+
+outputArray = []
+@metsManifests.sort.each {|mets| outputArray << parseMETS(mets)}
+
+File.open("#{target}/File-list.txt", "w+") do |f|
+  outputArray.each { |line| f.puts(line) }
+end
